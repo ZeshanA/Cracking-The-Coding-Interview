2a0d04cbfa22cc02985953618588f8bfeb017d35
Zeshan Amjad <zeshan@zesh.me>
2017-09-15 14:47:15 +0100

    Implement LinkedList insertion functions

--- Data Structures/linked_list.py
+++ Data Structures/linked_list.py
@@ -37,5 +37,37 @@ class LinkedList(object):
             post_node = cur_node.next_node
 
             prev_node.next_node = post_node
-            
+
         self.length -= 1
+
+    def insert(self, index, item):
+
+        if index == 0:
+            # inserting at the head
+            old_head = self.head
+            self.head = Node(item, old_head)
+        elif index == self.length - 1:
+            # inserting at end
+            self.__append_helper__(item)
+        else:
+            # inserting into middle
+            prev_node = self.get(index - 1)
+            cur_node = prev_node.next_node
+            prev_node.next_node = Node(item, cur_node)
+
+        self.length += 1
+
+    def last_node(self):
+        return self.get(self.length - 1)
+
+    def append(self, item):
+        self.__append_helper__(item)
+        self.length += 1
+
+    def __append_helper__(self, item):
+        new_node = Node(item, None)
+        if self.head is None:
+            # appending as the first item in the list
+            self.head = new_node
+        else:
+            self.last_node().next_node = new_node
