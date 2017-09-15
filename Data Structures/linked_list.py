8fb2a8db8b4ad2f312568288435afd3f1c356270
Zeshan Amjad <zeshan@zesh.me>
2017-09-15 14:44:04 +0100

    Create skeleton for LinkedList class

--- Data Structures/linked_list.py
+++ Data Structures/linked_list.py
@@ -1,5 +1,10 @@
 class Node(object):
-
     def __init__(self, item, next_node):
         self.item = item
         self.next_node = next_node
+
+
+class LinkedList(object):
+    def __init__(self):
+        self.head = None
+        self.length = 0
