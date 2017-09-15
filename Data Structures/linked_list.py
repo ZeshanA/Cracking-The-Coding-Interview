638df69a4097e6fca65fbb75e2fe233870993677
Zeshan Amjad <zeshan@zesh.me>
2017-09-15 14:44:52 +0100

    Implement LinkedList.get

--- Data Structures/linked_list.py
+++ Data Structures/linked_list.py
@@ -8,3 +8,12 @@ class LinkedList(object):
     def __init__(self):
         self.head = None
         self.length = 0
+
+    def get(self, index):
+        if index == 0:
+            return self.head
+        else:
+            cur_node = self.head
+            for i in range(0, index):
+                cur_node = cur_node.next_node
+            return cur_node
